dea8c9692ce72ab2dca4a42bc386aadab614ca5c
abdallah-Shokry <abdallah.shokry@oneteam.plus>
2024-11-25 12:32:20 +0200

    Initial commit

---
+++ src/main/java/com/example/OauthProjectWithGoogelAndGithub/OauthProjectWithGoogelAndGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.OauthProjectWithGoogelAndGithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthProjectWithGoogelAndGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthProjectWithGoogelAndGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OauthProjectWithGoogelAndGithub/OauthProjectWithGoogelAndGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OauthProjectWithGoogelAndGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthProjectWithGoogelAndGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
